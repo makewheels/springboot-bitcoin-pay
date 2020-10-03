3ec3540a22a7a990eecf9361c7e49c3dfc429cf9
makewheels <makewheels>
2020-10-03 17:33:07 +0800

    init

---
+++ src/main/java/com/eg/testbitcoinpay/TestBitcoinPayApplication.java
@@ -0,0 +1,13 @@
+package com.eg.testbitcoinpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestBitcoinPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestBitcoinPayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eg/testbitcoinpay/TestBitcoinPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eg.testbitcoinpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestBitcoinPayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
